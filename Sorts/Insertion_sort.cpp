5919c6c54ea590a3d42e0f2504c1c9c55a67499c
sergej <william_olford@mail.ru>
2024-03-22 21:51:59 +0700

    mmm classic

--- Sorts/Insertion_sort.cpp
+++ Sorts/Insertion_sort.cpp
@@ -1,6 +1,25 @@
 //
 // Created by Сергей on 22.03.2024.
 //
+#include "iostream"
+#include "vector"
+using namespace std;
+
 int main() {
+    int n;
+    cin >> n;
+    vector<int> num(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> num[i];
+    }
+    for (int i = 1; i < n; ++i) {
+        int pos = i;
+        while (pos > 0 && num[pos-1] > num[pos]) {
+            swap(num[pos], num[pos-1]);
+            pos--;
+        }
+    }
+    for (int c : num)
+        cout << c << " ";
 
 }
